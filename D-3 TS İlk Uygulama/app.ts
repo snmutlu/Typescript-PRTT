cf5e4d17a158699d6f467225fd5b2ffdcd6e1112
Mutlu SENEM <info.mutlusenem@gmail.com>
2024-04-29 20:32:33 +0300

    D-3 Upload

---
+++ D-3 TS İlk Uygulama/app.ts
@@ -0,0 +1,15 @@
+//Ders Linki = https://www.youtube.com/watch?v=UgxgqB34lq0&list=PLfAfrKyDRWrHHC_routvb5WEviGZbJA0w&index=3
+
+
+
+let message:string = "Hello World";
+
+const h1 = document.createElement("h1");
+
+h1.textContent = message;
+
+document.body.appendChild(h1);
+
+
+/* TS kodlarımızı yazdıktan sonra terminal üzerinden tsc app.ts komutu ile TS dosyamızı
+js dosyası olarak compile edebiliyoruz. */
