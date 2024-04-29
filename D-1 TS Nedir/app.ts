8fed53ba8f8ef75fdabd32a04e0cc978b4d1a733
Mutlu SENEM <info.mutlusenem@gmail.com>
2024-04-29 20:14:11 +0300

    First Commit

---
+++ D-1 TS Nedir/app.ts
@@ -0,0 +1,16 @@
+/* Ders linki = https://www.youtube.com/watch?v=yl46ucRsJE8&list=PLfAfrKyDRWrHHC_routvb5WEviGZbJA0w */
+
+/* Typescript, Javascript'in üst kümesidir. (super set)
+
+Typescript kodunuzu yazdıktan sonra kodunuzu Typescript compiler ile javascript koduna çevirirsiniz.
+
+Nihayetinde her zaman elinizde bir javascript kodunuz olur ve bunu istediğiniz yere
+ deploy edip kullanabilirsiniz.
+
+Typescript dosyaları javascript'in aksine .js yerine .ts uzantısını kullanır.  
+
+Typescript'i genel olarak tip tanımlama amacıyla kullanıyoruz diyebiliriz. 
+
+Ayrıca Typescriptte yazdığımız kodlardaki hataları runtime'da almak yerine 
+
+compile ederken yakalayabiliriz.*/
---
+++ D-2 TS Kurulumu/app.ts
@@ -0,0 +1,32 @@
+/* Ders Linki = https://www.youtube.com/watch?v=w95wP6azdRU&list=PLfAfrKyDRWrHHC_routvb5WEviGZbJA0w&index=4 */
+
+/* TypeScript yazmaya başlamak için gerekli araçların kurulumu:
+
+Node.js - TypeScript compiler'ı çalıştırmak için gerekli program. Nodejs bilmenize gerek yok.
+TypeScript Compiler - TypeScript'den JavaScript'e compile etmek için gerekli modül. Eğer nodejs'de 
+TypeScript kullanıyorsanız ts-node paketini kurabilirsiniz.
+VSCode - TypeScript'i destekleyen bir kod editörü. Önerilir, ancak herhangi bri kod editörü de 
+kullanabilirsiniz.
+Eğer VSCode kullanıyorsanız, geliştirme sürecinizi hızlandırmak için aşağıdaki eklentiyi de 
+yükleyebilirsiniz:
+
+Live Server - Değişiklikleri anında görebilmenizi sağlayan local bir server kurar.
+Node.js Kurulumu
+Node.js'i yüklemek için şu adımları takip edin:
+
+Node.js indirme sayfasına gidin
+İşletim sisteminize uygun olan sürümünü indirin.
+İnen dosyayı çalıştırıp kurulumu gerçekleştirin.
+Kurulumu doğrulamak için terminalinizi açıp node -v komutunu çalıştırın, eğer bir sürüm bilgisi 
+alıyorsanız başarıyla yüklenmiştir.
+TypeScript Compiler Kurulumu
+TypeScript Compiler'ı kurmak için, terminalinizi açın ve aşağıdaki komutu çalıştırın.
+
+npm install -g typescript
+Yükleme bittikten sonra, yüklendiğini doğrulmak için versiyon kontrolü yapabilirsiniz.
+
+tsc --v
+ts-node paketini global olarak yüklemek için şu komutu çalıştırabilirsiniz:
+
+npm install -g ts-node
+ */
